64c47ec23465c19bfee2f5ecf5af615da4608a3f
ABHIII777 <noob.ssooff@gmail.com>
2026-04-08 02:21:45 +0530

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css"
+import CalenderCard from "./ui/CalenderCard";
 
 export default function App() {
   return (
-    <>
-      
-    </>
+    <div className="flex h-screen items-center justify-center">
+      {/* <h1 className="text-4xl font-bold">Welcome to WallCalender</h1> */}
+      <CalenderCard />
+    </div>
   );
 }
--- src/ui/CalenderCard.tsx
+++ src/ui/CalenderCard.tsx
@@ -1,8 +1,14 @@
 import { Card } from "../components/ui/card";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import CalenderSection from "./CalenderSection";
 import ImageSection from "./ImageSection";
 
 export default function CalenderCard() {
+
+    
+
+
     return (
         <Card className="w-[900px] h-[500px] overflow-hidden rounded-2xl shadow-xl flex">
             <div className="w-1/2 bg-white p-8 relative">
