bc5fa75dbaba51f029952e0e6349a50564d5d26b
ABHIII777 <noob.ssooff@gmail.com>
2026-04-08 02:09:42 +0530

    Calender section and Image section source code

--- src/ui/CalenderSection.tsx
+++ src/ui/CalenderSection.tsx
@@ -1,7 +1,27 @@
 export default function CalenderSection () {
     return (
-        <div>
+        <div className="h-full flex flex-col justify-between">
+            <div>
+                <div className="flex item-denter gap-2">
+                    <span className="bg-yellow-400 text-white px-3 py-1 text-sm font-bold">01</span>
+                    <div>
+                        <h2 className="text-gray-700 font-semibold">2026</h2>
+                        <p className="text-gray-400 text-sm">January</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="grid grid-cols-7 gap-2 test-center text-gray600 text-sm">
+                {["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"].map(d => (
+                    <div className="font-medium" key={d}>{d}</div>
+                ))}
 
+                {
+                    Array.from({length: 31}, (_, i) => (
+                        <div key={i} className="py-1 hover:text-yellow-500 cursor-pointer">{i + 1}</div>
+                    ))
+                }
+            </div>
         </div>
     );
 }
--- src/ui/ImageSection.tsx
+++ src/ui/ImageSection.tsx
@@ -1,5 +1,33 @@
-export default function ImageSection () {
-    return(
-        <></>
-    );
+export default function ImageSection() {
+  return (
+    <div className="w-full h-full relative">
+
+      <img
+        src="https://images.unsplash.com/photo-1501785888041-af3ef285b470"
+        className="w-full h-full object-cover"
+      />
+
+      <div
+        className="absolute inset-0 bg-white"
+        style={{
+          clipPath: "polygon(0 0, 60% 0, 30% 100%, 0% 100%)"
+        }}
+      />
+
+      <div
+        className="absolute bg-yellow-400 w-32 h-32 top-0 left-0"
+        style={{
+          clipPath: "polygon(0 0, 100% 0, 0 100%)"
+        }}
+      />
+
+      <div className="absolute bottom-6 right-6 bg-white p-4 shadow-lg rounded-lg w-48">
+        <h3 className="font-bold text-gray-700">Company Name</h3>
+        <p className="text-xs text-gray-500 mt-2">
+          info@company.com
+        </p>
+      </div>
+
+    </div>
+  )
 }
